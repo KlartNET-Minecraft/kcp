9f313bef4c0548097804597deba788566004655e
JadeMin <36400787+JadeMin@users.noreply.github.com>
2026-09-14 15:32:40 +0900

    feat: add up/down sound tricky simulation

--- src/main/kotlin/io/klartnet/kcp/game/FootStep.kt
+++ src/main/kotlin/io/klartnet/kcp/game/FootStep.kt
@@ -18,7 +18,7 @@ fun EventNode<InstanceEvent>.addFootstepListeners(game: GameInstance) {
 		val player = event.player
 		if (!game.isAlive(player)) return@addListener
 		if (player.isSneaking && !player.isOnGround) return@addListener
-
+		
 		val dx = event.newPosition.x - player.position.x
 		val dz = event.newPosition.z - player.position.z
 		val dist = hypot(dx, dz)
@@ -41,15 +41,25 @@ fun EventNode<InstanceEvent>.addFootstepListeners(game: GameInstance) {
 		if (block.air()) return@addListener
 		
 		val blockSound = block.blockSoundType()?.stepSound() ?: SoundEvent.BLOCK_ANVIL_BREAK
-		val pitch = 0.9f + Random.nextFloat() * 0.2f
-		player.instance.playSoundExcept(
-			player,
-			Sound.sound(
-				blockSound,
-				Sound.Source.PLAYER,
-				1.0f, pitch
-			),
-			event.newPosition
-		)
+		val basePitch = 0.9f + Random.nextFloat() * 0.2f
+		game.instance.players.forEach { listener ->
+			if (listener != player) return@addListener
+			
+			val dy = event.newPosition.y - listener.position.y
+			val pitch = when {
+				dy > 1.5 -> basePitch * 0.7f
+				dy < -1.5 -> basePitch * 1.3f
+				else -> basePitch
+			}
+			
+			player.instance.playSound(
+				Sound.sound(
+					blockSound,
+					Sound.Source.PLAYER,
+					1.0f, pitch
+				),
+				event.newPosition
+			)
+		}
 	}
 }
